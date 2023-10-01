318cba1903e8ae08ea8930e4fc90b3e954b4ec92
DeveloperFO <48652952+DeveloperFO@users.noreply.github.com>
2023-10-01 10:28:34 +0200

    Create quick_sort.d

---
+++ quick_sort.d
@@ -0,0 +1,29 @@
+import std.stdio;
+
+void quickSort(T)(T[] arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pivotIndex = partition(arr, low, high);
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+int partition(T)(T[] arr, int low, int high)
+{
+    T pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
